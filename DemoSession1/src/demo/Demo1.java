edaa20802279cb635f776b152e9c7a40a812075d
DUY2602 <duydo7779@gmail.com>
2025-12-23 18:29:25 +0700

    Initial commit ignoring metadata

---
+++ DemoSession1/src/demo/Demo1.java
@@ -0,0 +1,20 @@
+// Package name needs to be lowercase & unique 
+package demo;
+
+// Class name should be written in CamelCase & unique
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// Printing in Java
+		System.out.println("what sup !!!");			// println contains \n at the end
+		
+		System.out.print("Hi there");
+		
+		System.out.println(); 				// Auto print \n
+		
+		System.out.print("Hello Java");
+	}
+}
+
+
+// Execute by pressing PLAY button or ctrl + F11
---
+++ DemoSession1/src/demo/Demo2.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo2 {
+	
+	// Quickly create main function (main + crtl space + ENTER)
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
