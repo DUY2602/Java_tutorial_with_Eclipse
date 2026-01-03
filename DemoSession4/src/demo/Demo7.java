c04c4dcc8f50da7d90845b6227c1c2588438b541
DUY2602 <duydo7779@gmail.com>
2026-01-03 17:52:13 +0700

    complete sorting array (buble sort)

--- DemoSession4/src/demo/Demo7.java
+++ DemoSession4/src/demo/Demo7.java
@@ -3,11 +3,83 @@ package demo;
 import java.util.Scanner;
 
 public class Demo7 {
+	/*
+	 * Create a program
+	 * 1. Prompt user for an array of integer (both the length and value)
+	 * 2. Find max, min value
+	 * 3. Calculate sum of values
+	 * 4. Calculate the average of positive values
+	 * 5. Count how many negative values
+	 * 6. Prompt user for an integer to check if it's in the array (if yes then  count the appearance)
+	 * 7. List all the prime numbers in the array
+	 * 8. Sort ascending (bubble sort)
+	 * 9. Sort descending (bubble sort)
+	 * */
 	public static void main(String[] args) {
-		arrayInput();
+		Scanner sc = new Scanner(System.in);
+		String key = "";
+		int[] a = arrayInput();
+		
+		do {
+			System.out.println("========Function Menu========");
+			System.out.println("1. Find max, min value");
+			System.out.println("2. Calculate sum values");
+			System.out.println("3. Calcualte average positive values");
+			System.out.println("4. Count how many negative values");
+			System.out.println("5. Check if integer is in array");
+			System.out.println("6. List all prime numbers");
+			System.out.println("7. Sort ascending");
+			System.out.println("8. Sort Descendinng");
+			
+			System.out.print("Enter your choice: ");
+			int choice = sc.nextInt();
+			
+			switch (choice) {
+				case 1:
+					minMax(a);
+					break;
+					
+				case 2:
+					sum(a);
+					break;
+
+				case 3:
+					average(a);
+					break;
+
+				case 4:
+					countNeg(a);
+					break;
+
+				case 5:
+					checkAppearance(a);
+					break;
+
+				case 6:
+					primeNum(a);
+					break;
+
+				case 7:
+					bubbleSortASC(a);
+					break;
+
+				case 8:
+					bubbleSortDESC(a);
+					break;
+
+				default:
+					System.out.println("Invalid choice !!!");
+					break;
+
+			}
+			sc.nextLine();
+			System.out.print("Do yo want to continue (Y/y): ");
+			key = sc.nextLine();
+			
+		} while(key.equalsIgnoreCase("y"));
 	}
 	
-	public static void arrayInput() {
+	public static int[] arrayInput() {
 		System.out.print("Enter the length of the array: ");
 		
 		Scanner sc = new Scanner(System.in);
@@ -18,34 +90,64 @@ public class Demo7 {
 			System.out.print("Enter the value for index " + i + ": ");
 			a[i] = sc.nextInt();
 		}
-		
+		return a;
+	}
+	
+	public static void minMax(int[] a) {
 		int min = a[0];
-		for(int i = 0; i < length; i++) {
+		for(int i = 0; i < a.length; i++) {
 			if (a[i] < min) {
 				min = a[i];
 			}
 		}
 		
 		int max = a[0];
-		for(int i = 0; i < length; i++) {
-			if (a[i] > min) {
+		for(int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
 				max = a[i];
 			}
 		}
 		
+		System.out.println("Min of array: " + min);
+		System.out.println("Max of array: " + max);
+	}
+		
+	public static void sum(int[] a) {
 		int sum = 0;
 		for(int num : a) {
 			sum += num;
 		}
 		
-		double average = sum / length;
+		System.out.println("Sum: " + sum);
+	}
+	
+	public static void average(int[] a) {
+		int sum = 0;
+		int count = 0;
+		for(int num : a) {
+			if(num > 0) {
+				sum += num;
+				count += 1;
+			}
+		}
 		
+		double average = sum / count;
+		System.out.println("Average: " + average);
+
+	}
+	
+	public static void countNeg(int[] a) {
 		int count_negative = 0;
 		for (int num : a) {
 			if (num < 0) {
 				count_negative += 1;
 			}
 		}
+		System.out.println("There are " + count_negative + " negative numbers");
+	}
+	
+	public static void checkAppearance(int[] a) {
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter a number to check: ");
 		int prompt = sc.nextInt();
@@ -57,7 +159,14 @@ public class Demo7 {
 				count_appearance += 1;
 			}
 		}
+		if (in == true) {
+			System.out.println(prompt + " is in the array and it appears " + count_appearance + " times");
+		} else {
+			System.out.println(prompt + " is not in the array");
+		}
+	}
 		
+	public static void primeNum(int[] a) {
 		int count_primenumbers = 0;
 		String prime_numbers = "";
 		for(int num : a) {
@@ -74,27 +183,43 @@ public class Demo7 {
 				}
 			}
 		}
-		
-		
-		
-		System.out.println("Min of array: " + min);
-		System.out.println("Max of array: " + max);
-		System.out.println("Sum: " + sum);
-		System.out.println("Average: " + average);
-		System.out.println("There are " + count_negative + " negative numbers");
-		
-		if (in == true) {
-			System.out.println(prompt + " is in the array and it appears " + count_appearance + " times");
-		} else {
-			System.out.println(prompt + " is not in the array");
-		}
-		
 		if (prime_numbers.trim() != "") {
 			System.out.println("There are " + count_primenumbers + " prime numbers: " + prime_numbers);
 		} else {
 			System.out.println("There are no prime numbers in the array");
 		}
+	}
+	
 		
-		// Sort giảm dần
+	public static void bubbleSortASC(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+		    for (int j = 0; j < a.length - i - 1; j++) {
+		        if (a[j] > a[j + 1]) {
+		        	
+		            int temp = a[j];
+		            a[j] = a[j + 1];
+		            a[j + 1] = temp;
+		        }
+		    }
+		}
+		for (int num : a) {
+			System.out.print(num + " ");
+		}
+	}
+	
+	public static void bubbleSortDESC(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+		    for (int j = 0; j < a.length - i - 1; j++) {
+		        if (a[j] < a[j + 1]) {
+		        	
+		            int temp = a[j];
+		            a[j] = a[j + 1];
+		            a[j + 1] = temp;
+		        }
+		    }
+		}
+		for (int num : a) {
+			System.out.print(num + " ");
+		}
 	}
 }
