1db8a1ac62c5f8834dc6f69e75f29dd818c534e2
DUY2602 <duydo7779@gmail.com>
2026-01-07 15:14:53 +0700

    do some exercises on Date type in Java

--- DemoSession6/src/demo/Demo7.java
+++ DemoSession6/src/demo/Demo7.java
@@ -1,6 +1,9 @@
 package demo;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Demo7 {
 	/*
@@ -25,7 +28,88 @@ public class Demo7 {
 	 * Convert to ngày 06, tháng 01, năm 2025 vào lúc 10 giờ 30 phút 30 giây
 	 * */
 	public static void main(String[] args) {
-		String s = "10/03/2025";
+		example1();
 		
+		System.out.println("\n=============================\n");
+		
+		example2();
+		
+		System.out.println("\n=============================\n");
+		
+		example3();
+		
+		System.out.println("\n=============================\n");
+		
+		example4();
+
+	}
+	
+	public static void example1() {
+		String s = "10/03/2025";
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = null;
+		
+		// Convert to Date type
+		try {
+			date = sdf.parse(s);
+			System.out.println(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		// Convert to year month day
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd");
+		String s2 = sdf2.format(date);
+		System.out.println(s2);
+	}
+	
+	public static void example2() {
+		Date today = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		
+		String s = sdf.format(today);
+		System.out.println(s);
+	}
+	
+	public static void example3() {
+		Scanner sc = new Scanner(System.in);
+		boolean isValid = true;
+		do {
+			System.out.print("Enter a date to check (dd/MM/yyyy): ");
+			String input = sc.nextLine();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			sdf.setLenient(false);
+			try {
+				Date date = sdf.parse(input);
+				SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+				
+				String s = sdf2.format(date);
+				System.out.println(s);
+				isValid = true;
+			} catch (Exception e) {
+				System.out.println("Invalid format !!!");
+				isValid = false;
+			}
+		} while (!isValid);
+	}
+	
+	public static void example4() {
+		 String s = "2025-01-06 10:30:30";
+		 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		 Date date = null;
+		 
+		 try {
+			date = sdf.parse(s);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 SimpleDateFormat sdf2 = new SimpleDateFormat("'ngày' dd, 'tháng' MM, 'năm' yyyy 'vào lúc' HH 'giờ' mm 'phút' ss 'giây'");
+		 String s2 = sdf2.format(date);
+		 
+		 System.out.println(s2);
 	}
 }
